0680175d5ef85d1bac7e23f58cd4509366b0fdf6
Your Name <you@example.com>
2022-12-18 21:38:05 +0530

    all files are added

---
+++ interface.py
@@ -0,0 +1,28 @@
+from flask import Flask ,jsonify ,request,render_template, redirect, url_for
+import config1
+import sklearn
+import pandas as pd
+import numpy as np
+from utils import CellPhonePrice
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_flask():
+
+    return render_template("index.html")
+
+@app.route('/predictedPrice',methods = ['GET','POST'])
+def prediction():
+    if request.method  == 'POST':
+        data = request.form
+        print('data : ',data)
+
+        phone_price = CellPhonePrice(data)
+        price = phone_price.get_predicted_price()
+        print ('*******',price)
+        return render_template("index.html", prediction= price)
+        # return jsonify({'price: ':price})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004)
